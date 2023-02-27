bacf0c03112d9bfabdf7451f7aed19bdf0dfd31c
Leon Remke <remke.lcode@gmail.com>
2023-02-27 09:33:11 +0100

    fix docker-compose

--- react_ageflix/vite.config.js
+++ react_ageflix/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    hmr: {
+        host: 'localhost'
+    },
+    port: 5173
+  }
 })
