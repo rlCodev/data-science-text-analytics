e8a9096500eb2394cb89e6527289b5acba7d07f1
Leon Remke <remke.lcode@gmail.com>
2023-03-02 09:05:30 +0100

    finish frontend finally

--- react_ageflix/src/components/movie-card-detail.jsx
+++ react_ageflix/src/components/movie-card-detail.jsx
@@ -1,13 +1,14 @@
 import { useLocation } from 'react-router-dom';
+import DoughnutChart from "../components/pie-chart";
 
 export default function MovieCardDetail() {
   const { state } = useLocation();
-  const  { tmdb_id, imdb_id, original_title, genres, tagline, overview, poster_path, pg_rating, id } = state;
+  const  { tmdb_id, imdb_id, original_title, genres, tagline, overview, poster_path, pg_rating, id, profanity_counts } = state;
   // genres = genres.map((genre) => {
   //     return genre.name;
   // });
   return (
-    <div className="max-h-80 p-8 flex space-x-8 text-black">
+    <div className="max-h-90 p-8 flex space-x-8 text-black">
       <div className="h-90 overflow-visible w-1/2">
         <img
           className="rounded-2xl shadow-lg"
@@ -27,6 +28,10 @@ export default function MovieCardDetail() {
        
         </div>
         <p className="text-lg text-gray-400 max-h-40 overflow-y-hidden">{overview}</p>
+        {profanity_counts && <div className="block">
+          <h2 className="text-lg text-gray-800 justify-start">Profane Word Frequencies</h2>
+          <DoughnutChart profanityCounts={profanity_counts}/>
+        </div>}
       </div>
     </div>
   );
